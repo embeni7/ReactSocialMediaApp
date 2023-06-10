022b707e7497aa942cc415b93098e0ea50b2e340
Emmanuel <emanuel7be@gmail.com>
2023-06-10 00:05:53 -0300

    componentes created

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,46 @@
-import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Outlet,
+} from "react-router-dom";
 import "./app.scss";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
+import Navbar from "./components/navBar/Navbar";
+import LeftBar from "./components/leftBar/LeftBar";
+import RightBar from "./components/rightBar/RightBar";
+import Home from "./pages/home/Home";
+import Profile from "./pages/profile/Profile";
 
 function App() {
+  const Layout = () => {
+    return (
+      <div>
+        <Navbar />
+        <div style={{ display: "flex" }}>
+          <LeftBar />
+          <Outlet />
+          <RightBar />
+        </div>
+      </div>
+    );
+  };
   const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/profile/:id",
+          element: <Profile />,
+        },
+      ],
+    },
     {
       path: "/login",
       element: <Login />,
